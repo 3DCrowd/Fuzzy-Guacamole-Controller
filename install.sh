935057ae92c150281da40fe2240126c4533c0e4c
Toby Johnson <toby.e.m.johnson@gmail.com>
2021-09-23 21:51:52 +0100

    Added mjpg streamer to install script

--- install.sh
+++ install.sh
@@ -55,4 +55,38 @@ echo "Changing hostname..."
 hostnamectl set-hostname fuzzyguacamole
 sed -i "6s/.*/127.0.1.1       fuzzyguacamole/" /etc/hosts
 
+echo "Installing mjpg_streamer..."
+apt-get install cmake libjpeg8-dev
+git clone https://github.com/jacksonliam/mjpg-streamer /home/pi/mjpg-streamer
+cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+make
+make install
+cd
+
+echo "Creating mjpg_streamer start script..."
+rm -f /usr/local/bin/startCamera.sh
+echo "#!/usr/bin/bash" >> /usr/local/bin/startCamera.sh
+echo "export LD_LIBRARY_PATH=/home/pi/mjpg-streamer/mjpg-streamer-experimental/." >> /usr/local/bin/startCamera.sh
+echo "/home/pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o 'output_http.so -w ./www' -i 'input_uvc.so'" >> /usr/local/bin/startCamera.sh
+chmod +x /usr/local/bin/startCamera.sh
+
+echo "Creating mjpg_streamer service..."
+rm -f /etc/systemd/system/startCameraStreamer.service
+echo "[Unit]" >> /etc/systemd/system/startCameraStreamer.service
+echo "Description=Connecting to the printer and starting the API" >> /etc/systemd/system/startCameraStreamer.service
+echo "After=network.target" >> /etc/systemd/system/startCameraStreamer.service
+echo "StartLimitIntervalSec=0" >> /etc/systemd/system/startCameraStreamer.service
+
+echo "[Service]" >> /etc/systemd/system/startCameraStreamer.service
+echo "Type=simple" >> /etc/systemd/system/startCameraStreamer.service
+echo "User=pi" >> /etc/systemd/system/startCameraStreamer.service
+echo "ExecStart=/usr/local/bin/startCamera.sh" >> /etc/systemd/system/startCameraStreamer.service
+echo "Restart=on-failure" >> /etc/systemd/system/startCameraStreamer.service
+echo "RestartSec=10s" >> /etc/systemd/system/startCameraStreamer.service
+
+echo "[Install]" >> /etc/systemd/system/startCameraStreamer.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/startCameraStreamer.service
+systemctl enable startCameraStreamer.service
+
 echo "Installation Complete. Please restart before using."
+
