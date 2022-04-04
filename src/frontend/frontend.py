6216338a00a3cdea0627d93c53959d226d49c955
fishbigger <toby.e.m.johnson@gmail.com>
2022-04-04 14:55:19 +0100

    Updated dashboard to run from flask Logs are now loaded with python not php.

---
+++ src/frontend/frontend.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/logs', methods=['GET'])
+def logs():
+    try:
+        with open("/var/www/html/printer.log") as f:
+            printerLogs = f.readlines()
+            printerLogs.reverse()
+            print (printerLogs)
+    except Exception as e:
+        return render_template('logs.html', error=True, message=e)
+
+    return render_template('logs.html', error=False, printerLogs=printerLogs)
+
+app.run()
